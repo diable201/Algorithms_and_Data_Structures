fda31d2fea769d29e8a981f51c8a1bc34977642b
Sanzhar <diable201@protonmail.com>
2020-10-16 19:26:58 +0600

    Start of lab Heap

--- informatics_4/1.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  1.cpp
-//  
-//
-//  Created by Sanzhar Seitbekov on 16.10.2020.
-//
-
-#include <stdio.h>
---
+++ informatics_4/sort.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MinHeap {
+    public:
+    vector<int> a;
+
+    int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    int left(int i) {
+        return 2 * i + 1;
+    }
+
+    int right(int i) {
+        return 2 * i + 2;
+    }
+
+    void insert(int k) {
+        a.push_back(k);
+        int i = a.size() - 1;
+        while (i > 0 && a[parent(i)] > a[i]) {
+            swap(a[parent(i)], a[i]);
+            i = parent(i);
+        }
+    }
+
+    void heapify(int i) {
+        if (left(i) > a.size() - 1)
+            return;
+        
+        int j = left(i);
+        if (right(i) < a.size() && a[right(i)] < a[left(i)])
+            j = right(i);
+
+        if (a[i] > a[j]) {
+            swap(a[i], a[j]);
+            heapify(j);
+        }
+    }
+
+    int extractMin() {
+        int root = a[0];
+        swap(a[0], a[a.size() - 1]);
+        a.pop_back();
+        if (a.size() > 0)
+            heapify(0);
+        return root;
+    }
+
+    void decreaseKey(int i, int new_value) {
+        a[i] = new_value;
+        while (i > 0 && a[parent(i)] > a[i]) {
+            swap(a[parent(i)], a[i]);
+            i = parent(i);
+        }
+    }
+
+    void increaseKey(int i, int new_value) {
+        a[i] = new_value;
+        heapify(i);
+    }
+
+    void print() {
+        for (int i = 0; i < a.size(); i++) 
+            cout << a[i] << " ";
+        cout << endl;
+    }
+
+    int getMin() {
+        return a[0];
+    }
+};
+
+int main() {
+    MinHeap *min_heap = new MinHeap();
+    uint32_t n;
+    cin >> n;
+    vector<int> sorted;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        min_heap->insert(k);
+    }
+
+    for (int i = 0; i < n; i++) 
+        sorted.push_back(min_heap->extractMin());        
+
+    for (int i = 0; i < n; i++)
+        cout << sorted[i] << " ";
+    cout << '\n';
+    return 0;
+}
