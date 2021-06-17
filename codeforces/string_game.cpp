2a5d5fb2218db5884eb480c47e5432f3f5bdb962
Sanzhar <diable201@protonmail.com>
2021-06-17 18:22:07 +0600

    Convert Indentation to Spaces

--- codeforces/string_game.cpp
+++ codeforces/string_game.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 int main() {
-	char s; 
+    char s; 
     int c = 0;
-	stack<char> x;
-	while ((s = getchar()) != '\n') {
-		if (!x.empty() && x.top() == s)
+    stack<char> x;
+    while ((s = getchar()) != '\n') {
+        if (!x.empty() && x.top() == s)
             c++, x.pop();
         else 
             x.push(s);
-	}
+    }
     c % 2 ? cout << "Yes\n" : cout << "No\n";
+    return 0;
 }
